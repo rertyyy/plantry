654c254fbae14f1f341038bd39decc6015ed1e25
rertyyy <rettabkawtar@gmail.com>
2025-09-12 13:54:18 +0200

    Create Homepage.tsx

---
+++ src/pages/Homepage.tsx
@@ -0,0 +1,11 @@
+// src/pages/Homepage.tsx
+import React from "react";
+
+export default function Homepage() {
+  return (
+    <div>
+      <h1>Welcome to the Homepage</h1>
+      <p>This is your starting page.</p>
+    </div>
+  );
+}
